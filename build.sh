60be9ef35cc3fdd53873b81412fb1c2f80d5e3db
wawireinnocent-blip <wawireinnocent@gmail.com>
2026-09-22 00:40:59 +0300

    create admin user

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
-
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python import_members.py || echo "import skipped - build continues"
+echo "from django.contrib.auth import get_user_model; User=get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin','admin@lutali.com','Admin@123')" | python manage.py shell
+python import_members.py || echo "import skipped"
