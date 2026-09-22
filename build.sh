b50a678f96f4436f29447c537682a84296a5671a
wawireinnocent-blip <wawireinnocent@gmail.com>
2026-09-22 09:06:57 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-echo "from django.contrib.auth import get_user_model; User=get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin','admin@lutali.com','Admin@123')" | python manage.py shell
-python import_members.py || echo "import skipped"
+python import_members.py
