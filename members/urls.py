fba63191ad1c6c7907eef54fd6c87a39f2e5b9d6
wawireinnocent-blip <wawireinnocent@gmail.com>
2026-09-22 11:33:55 +0300

    Update urls.py

--- members/urls.py
+++ members/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('member-login/', views.member_login, name='member_login'),
     path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('member-dashboard/', views.member_dashboard, name='member_dashboard'),
+    path('reports/', views.reports, name='reports'),
     path('logout/', views.member_logout, name='logout'),
     path('delete-member/<int:member_id>/', views.delete_member, name='delete_member'),
     path('import-now/', views.import_now, name='import_now'),
