752816370ef4cbbd1f1f6fe3b0a6f7ce9b292ded
wawireinnocent-blip <wawireinnocent@gmail.com>
2026-09-22 11:21:26 +0300

    Update urls.py

--- members/urls.py
+++ members/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.member_login, name='home'),
     path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('member-login/', views.member_login, name='member_login'),
+    path('member-dashboard/', views.member_dashboard, name='member_dashboard'),
+    path('logout/', views.member_logout, name='logout'),
     path('add-member/', views.add_member, name='add_member'),
     path('edit-member/<int:member_id>/', views.edit_member, name='edit_member'),
     path('delete-member/<int:member_id>/', views.delete_member, name='delete_member'),
